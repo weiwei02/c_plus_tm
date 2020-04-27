1523be936305651d679de60758f5ff64c809e61f
weiwei3.wang <weiwei3.wang@ly.com>
2020-04-27 21:42:49 +0800

    created simple libevent server test

--- src/sockets/source/event_simple_server.cc
+++ src/sockets/source/event_simple_server.cc
@@ -3,15 +3,16 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <unistd.h>
 
 #include "include/event.h"
 
 using namespace std;
 int tcp_server_init(int);
 void accept_cb(evutil_socket_t, short, void *);
+void socket_read_cb(evutil_socket_t, short, void *);
 
 int simple_server(int port){
-    
     int lfd = tcp_server_init(port);
     if (lfd <= 0)
     {
@@ -34,13 +35,19 @@ int simple_server(int port){
 // 初始化服务端
 int tcp_server_init(int port){
     int s_fd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_addr;
 
+    // 允许地址被多次绑定，需要声明在 socket 和 bind 之间
+    evutil_make_listen_socket_reuseable(s_fd);
+    struct sockaddr_in server_addr;
     // bind
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(port);
     bind(s_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    //跨平台统一接口，将套接字设置为非阻塞状态
+    evutil_make_socket_nonblocking(s_fd);
+
     if(listen(s_fd, 10) > -1){
         return s_fd;
     }
@@ -52,6 +59,41 @@ int tcp_server_init(int port){
  * 接收客户端新建连接方法
  * 监听接收到新连接后，会回调这个方法
 */
-void accept_cb(evutil_socket_t, short, void *){
+void accept_cb(evutil_socket_t l_fd, short, void * arg){
+    struct sockaddr_in client;
+    socklen_t len = sizeof(client);
+    evutil_socket_t sockefd = accept(l_fd, (struct sockaddr*)&client, &len);
+    evutil_make_socket_nonblocking(sockefd);
+    cout << "accept client socket fd " << sockefd << endl;
 
+    struct event_base *base = (event_base *) arg;
+    struct event *ev = event_new(NULL, -1, 0, NULL, NULL);
+    event_assign(ev, base, l_fd, EV_READ | EV_PERSIST, socket_read_cb, (void*) ev);
+
+    event_add(ev, NULL);
+}
+
+
+/**
+ * 读socket处理函数
+*/
+void socket_read_cb(evutil_socket_t fd, short, void * arg){
+    char msg[4096];
+    struct event *ev = (struct event*)arg;
+    int len = read(fd, msg, sizeof(msg) - 1);
+
+    if (len <= 0)
+    {
+        cout << "some error happen when read" << endl;
+        event_free(ev);
+        close(fd);
+        return;
+    }
+    msg[len] = '\0';
+    cout << "receive the client msg: " << msg << endl;
+    
+    char reply_msg[4096] = "I have received the msg: ";
+    strcat(reply_msg + strlen(reply_msg), msg);
+
+    write(fd, reply_msg, strlen(reply_msg));
 }
