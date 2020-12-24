7551222644be712cc1dbc6737ae65ba77dc56733
weiwei02 <weiwei02@vip.qq.com>
2020-12-24 16:48:45 +0800

    add doubly linked list

---
+++ goalgrithm/structs/containers/doubly_linked_list.go
@@ -0,0 +1,14 @@
+package containers
+
+import (
+	dll "github.com/emirpasic/gods/lists/doublylinkedlist"
+	"github.com/emirpasic/gods/utils"
+)
+
+func doublyLinkedlist(){
+	list := dll.New()
+	list.Add("a")
+	list.Add("c", "b", "a" , "q")
+	list.Sort(utils.StringComparator)
+	
+}
--- goalgrithm/structs/trees/b_tree.go
+++ goalgrithm/structs/trees/b_tree.go
@@ -7,7 +7,7 @@ import (
 
 func btree() {
 	tree := btree.NewWithIntComparator(3) // empty (keys are of type int)
-
+	delete()
 	tree.Put(1, "x") // 1->x
 	tree.Put(2, "b") // 1->x, 2->b (in order)
 	tree.Put(1, "a") // 1->a, 2->b (in order, replacement)
