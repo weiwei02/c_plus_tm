a69fc547e2b6eead16332878bf1c7d23bb56dc5e
weiwei02 <weiwei02@vip.qq.com>
2022-04-16 03:01:00 +0800

    增加最长非重复子串和子序列和最大值题目

---
+++ goalgrithm/dynamic/not_repeat_child_string.go
@@ -0,0 +1,29 @@
+package dynamic
+
+/**
+题目描述：
+输入一个字符串（只包含 a~z 的字符），求其最长不含重复字符的子字符串的长度。
+例如对于 arabcacfr，最长不含重复字符的子字符串为 acfr，长度为 4。
+
+解题思路：
+要求最长子串问题，即重复字符出现的最大间隔。
+设当前子串长度为l，最优解存在下面两个选择中
+1. 假如对字符 a，上次出现的位置是i，当前出现的位置是j，如果j-i大于l，则说明在l+1的范围内还未出现重复子串。否则应进入2
+2. 当前子串长度应该是j-i，即 [i +1, j]是当前的子串。
+
+*/
+
+func longest_no_repeat_child_string(str string) int {
+	array := make([]int, 26)
+	for i := 0; i < len(array); i++ {
+		array[i] = -1
+	}
+
+	// 当前子串长度
+	curLen := 0
+	// 最长子串长度
+	maxLen := 0
+	for i := 0; i < len(str); i++ {
+		asii := strconv.
+	}
+}
--- goalgrithm/go.mod
+++ goalgrithm/go.mod
@@ -1,6 +1,6 @@
 module github.com/weiwei02/c_plus_tm/goalgtithm
 
-go 1.14
+go 1.18
 
 require github.com/emirpasic/gods v1.12.0
 
